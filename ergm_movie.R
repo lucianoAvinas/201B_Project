6a7e3ef24be5b2c62f89a2e117b64355848e1300
lucianovinas <lucianovinas@gmail.com>
2021-02-19 01:20:18 -0800

    Cleaned up tabs

--- ergm_movie.R
+++ ergm_movie.R
@@ -5,38 +5,38 @@ load('movie_data.RData')
 
 
 nets_in_genre <- function(genre){
-	genre.info <- movies_by_genre[[genre]]
-	genre.nets <- list()
-	i <- 1
-	for(net.info in genre.info){
-		net <- as.network(net.info[['adj_mat']], directed=F, matrix.type='a',
-			              ignore.eval=F, names.eval='e_weights')
-		net %v% 'ranks' <- net.info[['ranks']]
-		net %v% 'names' <- net.info[['names']]
+    genre.info <- movies_by_genre[[genre]]
+    genre.nets <- list()
+    i <- 1
+    for(net.info in genre.info){
+        net <- as.network(net.info[['adj_mat']], directed=F, matrix.type='a',
+                          ignore.eval=F, names.eval='e_weights')
+        net %v% 'ranks' <- net.info[['ranks']]
+        net %v% 'names' <- net.info[['names']]
 
-		genre.nets[[i]] <- net
-		i <- i + 1
-	}
+        genre.nets[[i]] <- net
+        i <- i + 1
+    }
     genre.nets
 }
 
 
 simulate_obs <- function(nets, sample_per_obs, feat_formula,
-	                     ref_formula, etak) {
-	summ_list <- list()
-	for (i in 1:length(nets)) {
-		sims <- simulate(feat_formula, coef=etak, nsim=sample_per_obs,
-			             response='e_weights', reference=ref_formula,
-			             basis=nets[[i]])
-		summ_list[[i]] <- summary(update(sims~., feat_formula), 
-			                      response='e_weights')
-	}
+                         ref_formula, etak) {
+    summ_list <- list()
+    for (i in 1:length(nets)) {
+        sims <- simulate(feat_formula, coef=etak, nsim=sample_per_obs,
+                         response='e_weights', reference=ref_formula,
+                         basis=nets[[i]])
+        summ_list[[i]] <- summary(update(sims~., feat_formula), 
+                                  response='e_weights')
+    }
     do.call(rbind, summ_list)
 }
 
 
 eta_traditional <- function(nets, sample_per_obs, feat_formula, ref_formula, 
-	                        etak, n_samplings, gradient_steps){
+                            etak, n_samplings, gradient_steps){
     etak <- etak / norm(etak, type='2')
 
     # Product of multiples obsv. in log turns into a sum
@@ -44,19 +44,19 @@ eta_traditional <- function(nets, sample_per_obs, feat_formula, ref_formula,
     # n at info matrix step, so might as well take average now
     g_obs <- rep(0,length(etak))
     for(i in 1:length(nets)){
-    	g_obs <- g_obs + summary(update(nets[[i]]~., feat_formula),
-    		                            response='e_weights')
+        g_obs <- g_obs + summary(update(nets[[i]]~., feat_formula),
+                                        response='e_weights')
     }
     g_obs <- g_obs/length(nets)
 
-	for (i in 1:n_samplings) {
-		Z <- simulate_obs(nets, sample_per_obs, feat_formula, ref_formula, 
-			              etak)
-		eta0 <- etak
-		for (j in 1:gradient_steps) {
-			eta_prev <- etak
+    for (i in 1:n_samplings) {
+        Z <- simulate_obs(nets, sample_per_obs, feat_formula, ref_formula, 
+                          etak)
+        eta0 <- etak
+        for (j in 1:gradient_steps) {
+            eta_prev <- etak
 
-			w <- Z %*% (etak-eta0)
+            w <- Z %*% (etak-eta0)
             w <- exp(w - max(w))
             w <- w/sum(w)
 
@@ -68,34 +68,34 @@ eta_traditional <- function(nets, sample_per_obs, feat_formula, ref_formula,
             etak <- c(etak + solve(I_hat) %*% (g_obs - wZ_colsum))
 
             etak <- etak / norm(etak, type='2')
-		}
-		cat('iter =',i,', eta = (',etak,')','\n')
-		cat('eta diff',norm(etak-eta0, type='2'),'\n')
-	}
-	# shuffle Z to remove temporal depedence of observations
-	list(eta=etak, last_sim=Z[sample(nrow(Z)),])
+        }
+        cat('iter =',i,', eta = (',etak,')','\n')
+        cat('eta diff',norm(etak-eta0, type='2'),'\n')
+    }
+    # shuffle Z to remove temporal depedence of observations
+    list(eta=etak, last_sim=Z[sample(nrow(Z)),])
 }
 
 
 eta_distributional <- function(nets, sample_per_obs, feat_formula, ref_formula, 
-	                           etak, learning_iter, gamma){
-	etak <- etak / norm(etak, type='2')
+                               etak, learning_iter, gamma){
+    etak <- etak / norm(etak, type='2')
 
-	# Product of multiples obsv. in log turns into a sum
+    # Product of multiples obsv. in log turns into a sum
     # Going through the math will ultimately have division by 
     # n at cov matrix step, so might as well take average now
     g_obs <- rep(0,length(etak))
     for(i in 1:length(nets)){
-    	g_obs <- g_obs + summary(update(nets[[i]]~., feat_formula),
-    		                            response='e_weights')
+        g_obs <- g_obs + summary(update(nets[[i]]~., feat_formula),
+                                        response='e_weights')
     }
     g_obs <- g_obs/length(nets)
 
-	# New sampling done for each iteration
-	for(i in 1:learning_iter){
-		Z <- simulate_obs(nets, sample_per_obs, feat_formula, ref_formula, 
-			              etak)
-		eta0 <- etak
+    # New sampling done for each iteration
+    for(i in 1:learning_iter){
+        Z <- simulate_obs(nets, sample_per_obs, feat_formula, ref_formula, 
+                          etak)
+        eta0 <- etak
 
         xi_sample_mean <- colMeans(Z)
         cov_matrix <- cov(Z)
@@ -108,19 +108,19 @@ eta_distributional <- function(nets, sample_per_obs, feat_formula, ref_formula,
         cat('eta diff',norm(etak-eta0, type='2'),'\n')
     }
     # shuffle Z to remove temporal depedence of observations
-	list(eta=etak, last_sim=Z[sample(nrow(Z)),])
+    list(eta=etak, last_sim=Z[sample(nrow(Z)),])
 }
 
 
 nllratio <- function(eta, nets.holdout, feat_formula, ref_formula, eta0=NULL){
-	if (is.null(eta0)) {
-		eta0 <- rep(0, length(eta))
-	}
+    if (is.null(eta0)) {
+        eta0 <- rep(0, length(eta))
+    }
 
-	all_obs <- list()
+    all_obs <- list()
     for(i in 1:length(nets.holdout)){
-    	all_obs[[i]] <- summary(update(nets[[i]]~., feat_formula),
-    		               response='e_weights')
+        all_obs[[i]] <- summary(update(nets[[i]]~., feat_formula),
+                           response='e_weights')
     }
     all_obs <- do.call(c, rbind)
     sum_obs <- rowSums(all_obs)
@@ -133,27 +133,27 @@ nllratio <- function(eta, nets.holdout, feat_formula, ref_formula, eta0=NULL){
 
 
 example_methods <- function(method) {
-	# Method takes int = 0 or 1
+    # Method takes int = 0 or 1
 
-	test.nets <- nets_in_genre('Drama')[1:10]
-	for (i in 1:length(test.nets)) {
-		char_groups <- cut(test.nets[[i]] %v% 'ranks', c(1,4,8,Inf), 
-			               include.lowest=T, ordered.result=T)
-		test.nets[[i]] %v% 'char_groups' <- as.integer(char_groups)
-	}
+    test.nets <- nets_in_genre('Drama')[1:10]
+    for (i in 1:length(test.nets)) {
+        char_groups <- cut(test.nets[[i]] %v% 'ranks', c(1,4,8,Inf), 
+                           include.lowest=T, ordered.result=T)
+        test.nets[[i]] %v% 'char_groups' <- as.integer(char_groups)
+    }
 
     # Absdiffcat takes categorical variables of 3 categories, so it needs 3 - 1 etas
     if (method == 0) {
-    	res <- eta_distributional(test.nets, 1000, 
-     	    ~absdiffcat('char_groups')+smallerthan(3)+nodesqrtcovar(TRUE)+sum+edges,
-    	    ~Poisson, rnorm(6), 10, 0.5)
+        res <- eta_distributional(test.nets, 1000, 
+            ~absdiffcat('char_groups')+smallerthan(3)+nodesqrtcovar(TRUE)+sum+edges,
+            ~Poisson, rnorm(6), 10, 0.5)
     } else {
-    	res <- eta_traditional(test.nets, 1000, 
-     	    ~absdiffcat('char_groups')+smallerthan(3)+nodesqrtcovar(TRUE)+sum+edges,
-    	    ~Poisson, rnorm(6), 10, 500)
+        res <- eta_traditional(test.nets, 1000, 
+            ~absdiffcat('char_groups')+smallerthan(3)+nodesqrtcovar(TRUE)+sum+edges,
+            ~Poisson, rnorm(6), 10, 500)
     }
 
-	res
+    res
 }
 
 
